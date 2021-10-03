692364f974eb4f9592d4db16443c18bceef2c459
Johan <glowingthoughts@gmail.com>
2021-10-03 14:51:06 +0200

    Adding demand scaling

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,11 +28,17 @@ public class GameManager : MonoBehaviour
 
         [SerializeField, HideInInspector]
         private float m_CurrentExedeRate;
+        private float m_LastExedeRate;
 
         public ResourceType GetResourceType() { return m_ResourceType; }
         public float EvaluateTotalNeed(float time) { return m_Scaling.Evaluate(time / m_MaxCurveTime) * m_MaxNeed; }
         public float GetExedeRate() { return m_CurrentExedeRate; }
-        public void GenerateExedeRate() { m_CurrentExedeRate += Random.Range(m_MinExedeRate, m_MaxExedeRate); }
+        public void GenerateExedeRate()
+        {
+            m_LastExedeRate = m_CurrentExedeRate;
+            m_CurrentExedeRate += Random.Range(m_MinExedeRate, m_MaxExedeRate);
+        }
+        public float GetExedeDelta() { return m_CurrentExedeRate - m_LastExedeRate; }
     }
 
     //Editor variabels
@@ -53,8 +59,6 @@ public class GameManager : MonoBehaviour
         foreach (ResourceScaling scaling in m_ResourceScaling)
         {
             scaling.GenerateExedeRate();
-         //   Debug.Log("Exede Rate: " + scaling.GetExedeRate());
-
         }
     }
 
@@ -66,27 +70,25 @@ public class GameManager : MonoBehaviour
         {
             float total_resource_demand = scaling.EvaluateTotalNeed(m_Timer);
             float exede_rate = scaling.GetExedeRate();
-           // Debug.Log("Blue demand: " + total_resource_demand + ", Exede rate: " + scaling.GetExedeRate());
 
             if (total_resource_demand >= exede_rate)
             {
-                SpawnBuilding(scaling, total_resource_demand - exede_rate);
-
-                Debug.Log("Exede Rate: " + scaling.GetExedeRate());
+                SpawnBuilding(scaling);
             }
         }
 
         m_EntityManager.Tick();
     }
 
-    private void SpawnBuilding(ResourceScaling scaling, float delta_need)
+    private void SpawnBuilding(ResourceScaling scaling)
     {
 
         Entity spawned_entity = m_EntityManager.Add(m_DemandEntity, Vector3.zero);
         if (spawned_entity is Demand spawned_demand)
         {
-            Demand.Need need = new Demand.Need{ Type = scaling.GetResourceType(), Value = delta_need };
+            Demand.Need need = new Demand.Need{ Type = scaling.GetResourceType(), Value = scaling.GetExedeDelta() };
             spawned_demand.Needs.Add(need);
+            scaling.GenerateExedeRate();
         }
     }
 
