58f6fc15b12f3e66fe50aa767b9d74b47f276477
Johan <glowingthoughts@gmail.com>
2021-10-03 17:19:33 +0200

    wops long

--- Assets/Scripts/UI/WarningSystem.cs
+++ Assets/Scripts/UI/WarningSystem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,32 +6,41 @@ using UnityEngine;
 public class WarningSystem : MonoBehaviour
 {
     string prefabPath = "Prefabs/warning";
-    Dictionary<string, GameObject> warnings = new Dictionary<string, GameObject>();
+    Dictionary<int, GameObject> warnings = new Dictionary<int, GameObject>();
 
+    public static WarningSystem Instance;
 
     public GameObject testItem;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
     private void Start()
     {
         if(testItem != null)
         {
-            CreateWarningObject(testItem, "test", 60);
+            CreateWarningObject(testItem, -1, 60);
         }
     }
 
-    void CreateWarningObject(GameObject target, string id, int time)
+    public void CreateWarningObject(GameObject target, int id, float time)
     {
         GameObject obj = (GameObject)Instantiate(Resources.Load(prefabPath));
         obj.GetComponent<WarningObject>().constructor(target.transform, time);
         warnings.Add(id, target);
     }
 
-    void CancelWarningObject(string id)
+    public void CancelWarningObject(int id)
     {
         GameObject obj = warnings[id];
         Destroy(obj);
         warnings.Remove(id);
     }
 
-
-
+    internal bool ContainsWarning(int id)
+    {
+        return warnings.ContainsKey(id);
+    }
 }
