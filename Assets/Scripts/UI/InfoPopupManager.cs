42084425587d3e988c8cecc121b110f570a02124
NiklasLand <niklas-landberg@hotmail.com>
2021-10-04 15:23:31 +0200

    added nonefunctionaling infopopups

---
+++ Assets/Scripts/UI/InfoPopupManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InfoPopupManager : MonoBehaviour
+{
+
+    private string infoPopupPrefabPath = "prefabs/Infopopup";
+
+
+    public void AddInfo(string title, string body)
+    {
+        GameObject obj = (GameObject)Instantiate(Resources.Load(infoPopupPrefabPath));
+        obj.GetComponent<InfoPopup>().Init(title, body);
+    }
+}
