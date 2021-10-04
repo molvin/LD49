48238eac4d1abcbe8b63d6d3f8780097cb245fb6
molvin <akerstrom.perry@gmail.com>
2021-10-04 13:25:31 +0200

    asd

--- Assets/Extensions.cs
+++ Assets/Extensions.cs
@@ -11,21 +11,21 @@ public static class ResourceColor
         {
 
             case ResourceType.Cyan:
-                return new Color(constant, 1, 1);
+                return new Color(constant, 1, 1, 1);
             case ResourceType.Magenta:
-                return new Color(1, constant, 1);
+                return new Color(1, constant, 1, 1);
             case ResourceType.Yellow:
-                return new Color(1, 1, constant);
+                return new Color(1, 1, constant, 1);
             case ResourceType.Red:
-                return new Color(1, constant, constant);
+                return new Color(1, constant, constant, 1);
             case ResourceType.Green:
-                return new Color(constant, 1, constant);
+                return new Color(constant, 1, constant, 1);
             case ResourceType.Blue:
-                return new Color(constant, constant, 1);
+                return new Color(constant, constant, 1, 1);
             case ResourceType.Black:
-                return new Color(constant, constant, constant);
+                return new Color(constant, constant, constant, 1);
 
         }
-        return Color.white;
+        return new Color(0,0,0,0);
     }
 }
